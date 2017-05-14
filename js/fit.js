204a76f0d8d6d44ed1f8201c77264215845d0948
xinzai2014 <wanglianxin2016@sina.com>
2017-05-14 22:17:58 +0800

    updata code

--- js/fit.js
+++ js/fit.js
@@ -1,7 +1,24 @@
 var oA=new Audio();
 oA.src=oggSound['sound50'];
-setInterval(function(){
-    oA.play();
-},1000);
+var timer=null;
+var i=0;
+function toDou(i){
+    return i<10?'0'+i:''+i;
+}
+
+$('#btn').toggle(function(){
+    timer=setInterval(function(){
+        oA.play();
+        i++;
+        $('#view-num').text(toDou(i));
+        $('#btn').text('停止');
+    },1000);
+},function(){
+    i=0;
+    $('#view-num').text('00');
+    clearInterval(timer);
+    $('#btn').text('开始');
+});
+
 
 
